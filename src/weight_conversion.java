3ba8636f1114663603d6d2855b5e9c0d1f325fae
Supreet Bahoria <supreet.bahoria.gb@gmail.com>
2026-03-03 17:52:36 +0530

    4th proj

---
+++ src/weight_conversion.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class weight_conversion {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        double weight1;
+        double weight2;
+        int choice;
+        double result;
+
+        System.out.println("Weight conversion program");
+        System.out.println("1 : Convert kgs to grams ");
+        System.out.println("2 : Convert grams to kgs ");
+        System.out.print("Choose an option : ");
+        choice=sc.nextInt();
+
+        if (choice==1){
+            System.out.print("Enter weight in kgs : ");
+            weight1=sc.nextDouble();
+            result = weight1*1000;
+            System.out.println(weight1+" kg :- "+result+" g");
+
+        } else if (choice==2) {
+            System.out.print("Enter weight in grams : ");
+            weight2=sc.nextDouble();
+            result = weight2/1000;
+            System.out.printf("The weight in grams :- %.2f",result);
+
+        } else {
+            System.out.println("Invalid input please choose either of the choices.");
+        }
+
+        sc.close();
+    }
+}
