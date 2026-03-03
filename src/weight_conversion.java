8406a804afa0b08e4c8f4ee4483c310c9ffa7384
Supreet Bahoria <supreet.bahoria.gb@gmail.com>
2026-03-03 17:57:30 +0530

    4th prj

--- src/weight_conversion.java
+++ src/weight_conversion.java
@@ -17,13 +17,13 @@ public class weight_conversion {
             System.out.print("Enter weight in kgs : ");
             weight1=sc.nextDouble();
             result = weight1*1000;
-            System.out.println(weight1+" kg :- "+result+" g");
+            System.out.printf("The weight in grams :- %.2f",result);
 
         } else if (choice==2) {
             System.out.print("Enter weight in grams : ");
             weight2=sc.nextDouble();
             result = weight2/1000;
-            System.out.printf("The weight in grams :- %.2f",result);
+            System.out.printf("The weight in kg :- %.2f",result);
 
         } else {
             System.out.println("Invalid input please choose either of the choices.");
